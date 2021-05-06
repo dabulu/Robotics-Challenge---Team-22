edaaa9e7cae1771bd7414ac711f508031e56c2ab
Anthony Chatfield <achatfield1@sheffield.ac.uk>
2021-05-06 17:10:59 +0100

    colour_search now turns from x to find target, not yet finished 90degree turn followed by 180degree turn

--- src/colour_search.py
+++ src/colour_search.py
@@ -33,9 +33,11 @@ class colour_search(object):
 
         self.move_rate = '' # fast, slow or stop
         self.stop_counter = 0
+        self.current_yaw = 0.0
 
         self.robot_controller = MoveTB3()
         self.robot_odom = TB3Odometry()
+        self.complete = False
 
         self.ctrl_c = False
         rospy.on_shutdown(self.shutdown_ops)
@@ -67,9 +69,9 @@ class colour_search(object):
 
         lower = (115, 224, 100)
         upper = (130, 255, 255)
-        #[cyan, red, green, yellow, blue, magenta]
-        #[ 0  ,  1 ,   2  ,   3   ,  4  ,    5   ]
-        mask = colourMasks.getMask(hsv_img, 0)
+        #[turquoise, red, green, yellow, blue, magenta]
+        #[     0  ,   1 ,   2  ,   3   ,  4  ,    5   ]
+        mask = colourMasks.getMask(hsv_img, 5)
         #mask = cv2.inRange(hsv_img, lower, upper)
         res = cv2.bitwise_and(crop_img, crop_img, mask = mask)
 
@@ -83,37 +85,61 @@ class colour_search(object):
         cv2.imshow('cropped image', crop_img)
         cv2.waitKey(1)
 
-    def main(self):
-        while not self.ctrl_c:
-            if self.stop_counter > 0:
-                self.stop_counter -= 1
+    def set_robot_turning(self, turning_right):
+        if turning_right:
+            self.turn_vel_slow = -0.1
+            self.turn_vel_fast =  -0.5
+        else:
+            self.turn_vel_slow = 0.1
+            self.turn_vel_fast = 0.5
+
+    def find_target_pillar(self, target):
+        while (self.current_yaw - self.robot_odom.yaw) < target:
+            if (self.current_yaw - self.robot_odom.yaw) < 0:
+                print("Yaw was less than {}".format((self.current_yaw - self.robot_odom.yaw)))
+                break
 
             if self.m00 > self.m00_min:
                 # blob detected
                 if self.cy >= 560-100 and self.cy <= 560+100:
-                    if self.move_rate == 'slow':
-                        self.move_rate = 'stop'
-                        self.stop_counter = 30
+                    # if self.move_rate == 'slow':
+                    # print("STOP")
+                    self.complete = True
+                    self.robot_controller.stop()
+                    break
                 else:
-                    self.move_rate = 'slow'
+                    # self.move_rate = 'slow'
+                    self.robot_controller.set_move_cmd(0.0, self.turn_vel_slow)
             else:
-                self.move_rate = 'fast'
-
-            if self.move_rate == 'fast':
-                print("MOVING FAST: I can't see anything at the moment, scanning the area...")
+                # self.move_rate = 'fast'
                 self.robot_controller.set_move_cmd(0.0, self.turn_vel_fast)
-            elif self.move_rate == 'slow':
-                print("MOVING SLOW: A blob of colour of size {:.0f} pixels is in view at y-position: {:.0f} pixels.".format(self.m00, self.cy))
-                self.robot_controller.set_move_cmd(0.0, self.turn_vel_slow)
-            elif self.move_rate == 'stop' and self.stop_counter > 0:
-                print("STOPPED: The blob of colour is now dead-ahead at y-position {:.0f} pixels... Counting down: {}".format(self.cy, self.stop_counter))
-                self.robot_controller.set_move_cmd(0.0, 0.0)
-            else:
-                print("MOVING SLOW: A blob of colour of size {:.0f} pixels is in view at y-position: {:.0f} pixels.".format(self.m00, self.cy))
-                self.robot_controller.set_move_cmd(0.0, self.turn_vel_slow)
+
+
+            print("Yaw: {}".format(self.robot_odom.yaw))
 
             self.robot_controller.publish()
             self.rate.sleep()
+        print("Finished loop")
+
+    def main(self):
+        while not self.ctrl_c:
+
+            #set current yaw
+            self.current_yaw = self.robot_odom.yaw
+            print("Current yaw: {}".format(self.current_yaw))
+
+            #set robot to turn right
+            self.set_robot_turning(True)
+            #try to find pillar turning right
+            self.find_target_pillar(90)
+            #otherwise turn left if not finished
+            if not self.complete:
+                self.set_robot_turning(False)
+                self.find_target_pillar(180)
+
+            if self.complete:
+                print("SEARCH COMPLETE: The robot is now facing the target pillar.")
+                break
 
 if __name__ == '__main__':
     search_ob = colour_search()
